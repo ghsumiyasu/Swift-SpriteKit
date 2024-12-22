c135ffb05a8ff35f34047f6b60cb92ed1deb763e
ghsumiyasu <ghsumiyasu@gmail.com>
2024-12-22 17:49:32 +0900

    Initial Commit

---
+++ Swift SpriteKitTests/Swift_SpriteKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_SpriteKitTests.swift
+//  Swift SpriteKitTests
+//
+//  Created by Desenvolvimento on 2024/12/22.
+//
+
+import Testing
+@testable import Swift_SpriteKit
+
+struct Swift_SpriteKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Swift SpriteKitUITests/Swift_SpriteKitUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Swift_SpriteKitUITestsLaunchTests.swift
+//  Swift SpriteKitUITests
+//
+//  Created by Desenvolvimento on 2024/12/22.
+//
+
+import XCTest
+
+final class Swift_SpriteKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
