f81f89638925af5f1f3d4d00903aa11e5586326d
ghsumiyasu <ghsumiyasu@gmail.com>
2025-01-29 18:48:55 +0900

    Inclusao do Componente Animation

--- Swift SpriteKit/Entities/PlayerEntity.swift
+++ Swift SpriteKit/Entities/PlayerEntity.swift
@@ -17,11 +17,21 @@ class PlayerEntity: GKEntity {
     
     override init() {
         super.init()
-        let node = SKSpriteNode(color: .orange, size: .init(width: 100, height: 100))
+        let node = SKSpriteNode(imageNamed: "idle1.png")
+        node.anchorPoint = .init(x: 0.46, y: 0)
+        node.setScale(2)
         self.addComponent(GKSKNodeComponent(node: node))
+        
+        let animationComp = AnimationComponent(
+            idleAction: .repeatForever(.animate(with: .init(withFormat: "idle%@.png",
+                range: 1...6), timePerFrame: 0.1)),
+            runAction: .repeatForever(.animate(with: .init(withFormat: "run%@.png",
+                range: 1...12), timePerFrame: 0.1)))
+        self.addComponent(animationComp)
         
         let moveComp = MovementComponent(speed: 5)
         self.addComponent(moveComp)
+        
     }
     
     required init?(coder: NSCoder) {
